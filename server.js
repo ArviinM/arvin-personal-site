4f43387e91023427c8f0d1bd77165e4025e4f14f
ArviinM <rhenmedina20@gmail.com>
2020-11-21 14:44:07 +0800

    Update removing certs and new port

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const https = require('https')
-const path = require('path')
-const fs = require('fs')
 const app = express()
-const port = 2053;
+const port = 39501;
 
 //Static Files
 app.use(express.static(__dirname + '/public'));
@@ -13,12 +10,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
-const sslServer = https.createServer({
-    key: fs.readFileSync(__dirname + '/cert/key.key'),
-    cert: fs.readFileSync(__dirname + '/cert/cert.crt')
-}, app)
-
-// listening port to 2053
+// listening port to 39501
 sslServer.listen(port, function(){
     console.log(`Server started on port ${port}`);
 });
