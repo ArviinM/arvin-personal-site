aba1814c4ad01688d5d3209f66b176d467ac5017
ArviinM <rhenmedina20@gmail.com>
2020-11-21 12:14:43 +0800

    Adding SSL

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const https = require('https')
+const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = 8443;
 
@@ -10,7 +13,12 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
+const sslServer = https.createServer({
+    key: fs.readFileSync(__dirname + '/cert/key.key'),
+    cert: fs.readFileSync(__dirname + '/cert/cert.crt')
+}, app)
+
 // listening port to 8443
-app.listen(port, function(){
+sslServer.listen(port, function(){
     console.log(`Server started on port ${port}`);
 });
